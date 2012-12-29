79347e7f6c0fc947d41bfaa6dd1882cf58a7752e
Kauhsa <kauhsa@viuhka.fi>
2012-12-29 13:40:31 +0200

    Test moving to walls

--- project/src/test/java/kauhsa/sokoban/core/WorldObjectMovementTest.java
+++ project/src/test/java/kauhsa/sokoban/core/WorldObjectMovementTest.java
@@ -3,7 +3,9 @@
  * and open the template in the editor.
  */
 package kauhsa.sokoban.core;
+import kauhsa.sokoban.core.worldobjects.Floor;
 import kauhsa.sokoban.core.worldobjects.Player;
+import kauhsa.sokoban.core.worldobjects.Wall;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -20,30 +22,42 @@ public class WorldObjectMovementTest {
     private Player player;
     private WorldMovementHandler worldMovementHandler;
     
+    
     @Before
     public void setUp() {
-        world = new World(3, 3);
+        world = new World(3, 3);        
+        fillWorldWithFloor();
         worldMovementHandler = new WorldMovementHandler(world);
         player = new Player();
-        world.placeWorldObject(player, new Point(1, 1));
-        
+        world.placeWorldObject(player, new Point(1, 1));        
     }
     
-    public void checkPlayerIsInPoint(Point point) {
+    private void fillWorldWithFloor() {
+        for (Point point : world.getPoints()) {
+            Floor floor = new Floor();
+            world.placeWorldObject(floor, point);
+        }
+    }
+    
+    private void placeWall(Point point) {
+        Wall wall = new Wall();
+        world.placeWorldObject(wall, point);
+    }
+    
+    private void checkPlayerIsInPoint(Point point) {
         boolean isPlayerInPoint = world.getWorldObjectsInPoint(point).contains(player);
         String assertMessage = String.format("Player should now be in %s", point);
-        assertTrue(assertMessage, isPlayerInPoint);
+        assertTrue(assertMessage, isPlayerInPoint);        
+        assertEquals(point, player.getPosition()); // Just to be extra sure
     }
     
-    public void checkMovementAndNewPoint(Direction direction, Point point) {
+    private void checkMovementAndNewPoint(Direction direction, Point point) {
         worldMovementHandler.move(player, direction);
         checkPlayerIsInPoint(point);
     }
     
     @Test
-    public void testWorldBounds() {
-        checkPlayerIsInPoint(new Point(1, 1));
-        
+    public void testWorldBounds() {       
         checkMovementAndNewPoint(Direction.LEFT, new Point(0, 1));        
         checkMovementAndNewPoint(Direction.LEFT, new Point(0, 1));        
         checkMovementAndNewPoint(Direction.UP, new Point(0, 0));
@@ -55,4 +69,29 @@ public class WorldObjectMovementTest {
         checkMovementAndNewPoint(Direction.DOWN, new Point(2, 2));
         checkMovementAndNewPoint(Direction.DOWN, new Point(2, 2));        
     }
+    
+    @Test
+    public void testMovingToWall() {
+        placeWall(new Point(0, 1));
+        
+        checkMovementAndNewPoint(Direction.LEFT, new Point(1, 1));        
+        checkMovementAndNewPoint(Direction.UP, new Point(1, 0));        
+        checkMovementAndNewPoint(Direction.LEFT, new Point(0, 0));        
+        checkMovementAndNewPoint(Direction.DOWN, new Point(0, 0)); 
+    }
+    
+    @Test
+    public void testMovingIfWallsEverywhere() { 
+        placeWall(new Point(0, 1));         
+        placeWall(new Point(2, 1));
+        placeWall(new Point(1, 0));        
+        placeWall(new Point(1, 2));
+        
+        checkMovementAndNewPoint(Direction.LEFT, new Point(1, 1));        
+        checkMovementAndNewPoint(Direction.UP, new Point(1, 1));        
+        checkMovementAndNewPoint(Direction.DOWN, new Point(1, 1));        
+        checkMovementAndNewPoint(Direction.RIGHT, new Point(1, 1)); 
+    }
+    
+    
 }
