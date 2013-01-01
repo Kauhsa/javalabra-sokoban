81a4a296ec8023d521ac91477247fb8e081aa201
Kauhsa <kauhsa@viuhka.fi>
2013-01-01 13:33:56 +0200

    Move test level to resources, broke level loading though

--- project/src/test/java/kauhsa/sokoban/yamllevel/YAMLLevelTest.java
+++ project/src/test/java/kauhsa/sokoban/yamllevel/YAMLLevelTest.java
@@ -3,6 +3,8 @@ package kauhsa.sokoban.yamllevel;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import kauhsa.sokoban.core.Level;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -15,13 +17,16 @@ import static org.junit.Assert.*;
  * @author mika
  */
 public class YAMLLevelTest {
+    private Level getYAMLLevelFromResource(String s) {
+        InputStream levelFile = YAMLLevelTest.class.getResourceAsStream(s);
+        return new YAMLLevel(levelFile);
+    }
+    
     @Test
-    public void fileLevelTest() throws FileNotFoundException {
-        File file = new File("levels/test_level.yaml");
-        FileInputStream fIS = new FileInputStream(file);
-        YAMLLevel fileLevel = new YAMLLevel(fIS);
-        System.out.println(fileLevel.getAuthor());
-        System.out.println(fileLevel.getName());
+    public void metaDataTest() throws FileNotFoundException {
+        Level level = getYAMLLevelFromResource("test_level.yaml");
+        assertEquals("Kauhsa", level.getAuthor());
+        assertEquals("TestLevel", level.getName());
     }
     
 }
